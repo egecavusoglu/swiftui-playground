a965eedeb0666d1f4a3fdca8a7bed6af1e2bdcb3
Ege Cavusoglu <egecavusoglu99@gmail.com>
2022-01-01 11:41:16 +0300

    Initial Commit

---
+++ Shared/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  Shared
+//
+//  Created by Ege Çavuşoğlu on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
